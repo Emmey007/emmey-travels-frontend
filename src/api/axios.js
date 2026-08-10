bf4065b926c7def3b903ef27432d257fe6004787
Emmanuel <emmeyemmanuel558@gmail.com>
2026-08-10 13:43:14 -0700

    Fix production API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,21 @@
 import axios from 'axios'
+
 const BASE_URL = 'https://emmey-travels.vercel.app/api/v1'
-export const publicInstance = axios.create({ baseURL: BASE_URL })
-export const privateInstance = axios.create({ baseURL: BASE_URL })
+
+export const publicInstance = axios.create({
+  baseURL: BASE_URL
+})
+
+export const privateInstance = axios.create({
+  baseURL: BASE_URL
+})
+
 privateInstance.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token')
-    if (token) config.headers.Authorization = `Bearer ${token}`
-    return config
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
 })
